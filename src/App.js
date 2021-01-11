f4f99dd31785cb9d65890c4d4fa7a259c6dc1ec7
arunank.sharan <arunank.sharan@ril.com>
2021-01-11 06:42:35 +0530

    Added aboutus page and secttion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
 
+// Pages
+import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
       <h1>Portfolio Project</h1>
+      <AboutUs />
     </div>
   );
 }
