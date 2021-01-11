b316fedac1906217f32691f5d027ce3601dddc96
arunank.sharan <arunank.sharan@ril.com>
2021-01-11 06:59:58 +0530

    AboutUs page completed

---
+++ src/components/FaqSection.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequatur, magnam? Maiores autem consequatur non, ullam debitis,
+            quas consequuntur laboriosam magni animi asperiores eaque ipsum,
+            dolore expedita explicabo obcaecati alias cupiditate?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequatur, magnam? Maiores autem consequatur non, ullam debitis,
+            quas consequuntur laboriosam magni animi asperiores eaque ipsum,
+            dolore expedita explicabo obcaecati alias cupiditate?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequatur, magnam? Maiores autem consequatur non, ullam debitis,
+            quas consequuntur laboriosam magni animi asperiores eaque ipsum,
+            dolore expedita explicabo obcaecati alias cupiditate?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Where can I see a sample of work?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequatur, magnam? Maiores autem consequatur non, ullam debitis,
+            quas consequuntur laboriosam magni animi asperiores eaque ipsum,
+            dolore expedita explicabo obcaecati alias cupiditate?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Is there any student discount?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequatur, magnam? Maiores autem consequatur non, ullam debitis,
+            quas consequuntur laboriosam magni animi asperiores eaque ipsum,
+            dolore expedita explicabo obcaecati alias cupiditate?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
